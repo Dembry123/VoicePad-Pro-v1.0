0adfba5c5fb76a8a40ed0c8b7b74e05f2c621bed
DukeBartholomew <ddbartholomew@mail.smu.edu>
2023-11-16 19:09:29 -0600

    "Button Changes"

--- MachineLearningLab/ViewController.swift
+++ MachineLearningLab/ViewController.swift
@@ -8,50 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var goodbyeButton: UIButton!
+    @IBOutlet weak var adiosButton: UIButton!
+    @IBOutlet weak var sayonaraButton: UIButton!
+    @IBOutlet weak var aurevoirButton: UIButton!
+    @IBOutlet weak var ciaoButton: UIButton!
+    @IBOutlet weak var microphoneImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.goodbyeButton.backgroundColor = UIColor.clear
+        
+        // Add a tap gesture recognizer for the microphoneImage
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
+            tapGestureRecognizer.numberOfTapsRequired = 1
+            tapGestureRecognizer.numberOfTouchesRequired = 1
+            microphoneImage.isUserInteractionEnabled = true
+            microphoneImage.addGestureRecognizer(tapGestureRecognizer)
 
         // Add targets for the button events
         self.goodbyeButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
         self.goodbyeButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
-            
+        
+        self.adiosButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
+        self.adiosButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
+        
+        self.sayonaraButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
+        self.sayonaraButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
+        
+        self.aurevoirButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
+        self.aurevoirButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
+        
+        self.ciaoButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
+        self.ciaoButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
+        
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchPoint = touch.location(in: view)
+            if microphoneImage.frame.contains(touchPoint) {
+                // Touch down on the microphoneImage, change color to red
+                microphoneImage.tintColor = UIColor(red: 0.7, green: 0, blue: 0, alpha: 1.0)
+            }
+        }
+    }
     
     @objc func buttonTouchDown(_ sender: UIButton) {
         // Button is being held down (touch down), change background color to red
-        sender.backgroundColor = UIColor.red
+        
+        print("ON \(sender.titleLabel?.text ?? "")")
     }
-
+    
     @objc func buttonTouchUpInside(_ sender: UIButton) {
         // Button is released (touch up), change background color back to normal
-        sender.backgroundColor = UIColor.clear // Set it to your default background color
-    }
-
-    
-    @IBAction func goodbyeTrain(_ sender: UIButton) {
-    
-
+        print("OFF \(sender.titleLabel?.text ?? "")")
     }
     
-    
-    @IBAction func adiosTrain(_ sender: Any) {
-    }
-    
-
-    @IBAction func sayonaraTrain(_ sender: Any) {
+    @objc func imageTapped(_ sender: UITapGestureRecognizer) {
+        if sender.state == .began {
+            // Touch down, change color to red
+            microphoneImage.tintColor = UIColor.red
+        } else if sender.state == .ended {
+            // Touch up, change color to blue
+            microphoneImage.tintColor = UIColor.link
+        }
     }
     
     
-    @IBAction func aurevoirTrain(_ sender: Any) {
-    }
     
-    
-    @IBAction func ciaoTrain(_ sender: Any) {
-    }
 }
-
