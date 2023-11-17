b0bf9f48f400188f07c554e09e0d75e7e7ef731a
DukeBartholomew <ddbartholomew@mail.smu.edu>
2023-11-16 18:20:26 -0600

    Initial Commit

--- MachineLearningLab/ViewController.swift
+++ MachineLearningLab/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var goodbyeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.goodbyeButton.backgroundColor = UIColor.clear
+
+        // Add targets for the button events
+        self.goodbyeButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
+        self.goodbyeButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
+            
+    }
+    
+    
+    @objc func buttonTouchDown(_ sender: UIButton) {
+        // Button is being held down (touch down), change background color to red
+        sender.backgroundColor = UIColor.red
     }
 
+    @objc func buttonTouchUpInside(_ sender: UIButton) {
+        // Button is released (touch up), change background color back to normal
+        sender.backgroundColor = UIColor.clear // Set it to your default background color
+    }
 
+    
+    @IBAction func goodbyeTrain(_ sender: UIButton) {
+    
+
+    }
+    
+    
+    @IBAction func adiosTrain(_ sender: Any) {
+    }
+    
+
+    @IBAction func sayonaraTrain(_ sender: Any) {
+    }
+    
+    
+    @IBAction func aurevoirTrain(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ciaoTrain(_ sender: Any) {
+    }
 }
 
