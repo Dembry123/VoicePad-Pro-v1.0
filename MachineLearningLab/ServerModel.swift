07f541924245fb2b571ecd1637ad2e3f8ee1c0bb
Dembry123 <dylanembry1418@gmail.com>
2023-11-20 19:23:00 -0600

    added comments

--- MachineLearningLab/ServerModel.swift
+++ MachineLearningLab/ServerModel.swift
@@ -9,13 +9,18 @@ import Foundation
 
 class ServerModel {
     
+    // Base URL of the server
     let SERVER_URL = "http://10.8.153.54:8000"
+    
+    // Variables to store the accuracy and prediction labels
     var accuracyLabel = ""
     var predictionLabel = ""
 
+    // Function to send FFT data and language label to server for adding a data point
     func sendPostRequest(fftData: [Float], languageLabel: String) {
         let baseURL = "\(SERVER_URL)/AddDataPoint"
         
+        // Ensure the URL is valid
         guard let url = URL(string: baseURL) else {
             print("Invalid URL")
             return
@@ -25,14 +30,17 @@ class ServerModel {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
-        let dsid = 0 // Replace with actual dsid value if necessary
+        // Replace dsid with the actual dataset id value as needed
+        let dsid = 0
 
+        // Prepare the post data
         let postData: [String: Any] = [
             "feature": fftData,
             "label": languageLabel,
             "dsid": dsid
         ]
 
+        // Serialize the data into JSON format
         guard let jsonData = try? JSONSerialization.data(withJSONObject: postData, options: []) else {
             print("Error: Unable to serialize JSON data")
             return
@@ -40,12 +48,14 @@ class ServerModel {
 
         request.httpBody = jsonData
 
+        // Create and start the data task
         let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {
                 print("Error: \(error?.localizedDescription ?? "Unknown error")")
                 return
             }
 
+            // Handle the response
             if let jsonString = String(data: data, encoding: .utf8) {
                 print("Response: \(jsonString)")
             } else {
@@ -56,9 +66,11 @@ class ServerModel {
         dataTask.resume() // Start the task
     }
 
+    // Function to send FFT data for making a prediction
     func sendPredictionRequest(fftData: [Float], completion: @escaping (String) -> Void){
-        let baseURL = "\(SERVER_URL)/PredictOne" // Replace '/upload' with the correct endpoint
+        let baseURL = "\(SERVER_URL)/PredictOne"
 
+        // Ensure the URL is valid
         guard let url = URL(string: baseURL) else {
             print("Invalid URL")
             return
@@ -68,13 +80,16 @@ class ServerModel {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
-        let dsid = 0 // Replace with actual dsid value if necessary
+        // Replace dsid with the actual dataset id value as needed
+        let dsid = 0
 
+        // Prepare the post data
         let postData: [String: Any] = [
             "feature": fftData,
             "dsid": dsid
         ]
 
+        // Serialize the data into JSON format
         guard let jsonData = try? JSONSerialization.data(withJSONObject: postData, options: []) else {
             print("Error: Unable to serialize JSON data")
             return
@@ -82,12 +97,14 @@ class ServerModel {
 
         request.httpBody = jsonData
 
+        // Create and start the data task
         let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {
                 print("Error: \(error?.localizedDescription ?? "Unknown error")")
                 return
             }
 
+            // Handle the response and execute completion handler
             if let jsonString = String(data: data, encoding: .utf8) {
                 print("Response: \(jsonString)")
                 completion(jsonString)
@@ -99,37 +116,11 @@ class ServerModel {
         dataTask.resume() // Start the task
     }
 
-//    func trainModels(){
-//        let baseURL = "\(SERVER_URL)/UpdateModel" // Append dsid as a query parameter
-//
-//        guard let url = URL(string: baseURL) else {
-//            print("Invalid URL")
-//            return
-//        }
-//
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//
-//        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
-//            guard let data = data, error == nil else {
-//                print("Error: \(error?.localizedDescription ?? "Unknown error")")
-//                return
-//            }
-//
-//            if let jsonString = String(data: data, encoding: .utf8) {
-//                print("Response: \(jsonString)")
-//                self.accuracyLabel = jsonString
-//            } else {
-//                print("Unable to convert data to UTF-8 string")
-//            }
-//        }
-//
-//        dataTask.resume() // Start the task
-//    }
-//    
+    // Function to initiate the model training on the server and handle the response
     func trainModels(completion: @escaping (String) -> Void){
-        let baseURL = "\(SERVER_URL)/UpdateModel" // Append dsid as a query parameter
+        let baseURL = "\(SERVER_URL)/UpdateModel" 
 
+        // Ensure the URL is valid
         guard let url = URL(string: baseURL) else {
             print("Invalid URL")
             return
@@ -138,21 +129,22 @@ class ServerModel {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
 
+        // Create and start the data task
         let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {
                 print("Error: \(error?.localizedDescription ?? "Unknown error")")
                 return
             }
 
+            // Handle the response and execute completion handler
             if let jsonString = String(data: data, encoding: .utf8) {
                 print("Response: \(jsonString)")
                 completion(jsonString)
             } else {
-                print("Unable to convert data to UTF-8 string")
+                print("Unable to convert data to UTF")
             }
-        }
 
+         }
         dataTask.resume() // Start the task
     }
-    
 }
--- MachineLearningLab/ViewController.swift
+++ MachineLearningLab/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionDelegate {
     
-    
-    
     // MARK: Class Properties
+    
+    // Create a URLSession with custom configuration and delegate
     lazy var session: URLSession = {
         let sessionConfig = URLSessionConfiguration.ephemeral
         
@@ -21,11 +21,13 @@ class ViewController: UIViewController, URLSessionDelegate {
         
         return URLSession(configuration: sessionConfig,
                           delegate: self,
-                          delegateQueue:self.operationQueue)
+                          delegateQueue: self.operationQueue)
     }()
+    
+    // Create an operation queue for handling URLSession delegate methods
     let operationQueue = OperationQueue()
     
-    
+    // Connect buttons from the Interface Builder
     @IBOutlet weak var goodbyeButton: UIButton!
     @IBOutlet weak var adiosButton: UIButton!
     @IBOutlet weak var sayonaraButton: UIButton!
@@ -36,42 +38,51 @@ class ViewController: UIViewController, URLSessionDelegate {
     @IBOutlet weak var finalPrediction: UILabel!
     @IBOutlet weak var predictButton: UIButton!
     
+    // Constants for audio buffer size
     struct AudioConstants{
         static let AUDIO_BUFFER_SIZE = 1024*4
     }
-    // setup audio model
+    
+    // Create an instance of the AudioModel class
     let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
+    
+    // Create an instance of the ServerModel class
     let server = ServerModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Add targets for button events
         
-        // Add targets for the button events
+        // Set up touch down and touch up events for the "Goodbye" button
         self.goodbyeButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
         self.goodbyeButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
         
+        // Set up touch down and touch up events for the "Adios" button
         self.adiosButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
         self.adiosButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
         
+        // Set up touch down and touch up events for the "Sayonara" button
         self.sayonaraButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
         self.sayonaraButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
         
+        // Set up touch down and touch up events for the "Aurevoir" button
         self.aurevoirButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
         self.aurevoirButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
         
+        // Set up touch down and touch up events for the "Ciao" button
         self.ciaoButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
         self.ciaoButton.addTarget(self, action: #selector(buttonTouchUpInside), for: .touchUpInside)
         
+        // Set up touch down and touch up events for the "Predict" button
         self.predictButton.addTarget(self, action: #selector(predicitonButtonTouchDown), for: .touchDown)
         self.predictButton.addTarget(self, action: #selector(predictButtonTouchUp), for: .touchUpInside)
         
+        // Set up touch down event for the "Train" button
         self.trainButton.addTarget(self, action: #selector(trainButtonTouchDown), for: .touchDown)
-        
-        
-        
     }
     
+    // Function to handle the touch down event for the "Train" button
     @objc func trainButtonTouchDown(_ sender: UIButton){
         self.server.trainModels { [weak self] accuracy in
             DispatchQueue.main.async {
@@ -88,35 +99,35 @@ class ViewController: UIViewController, URLSessionDelegate {
         }
     }
     
-    
+    // Function to handle the touch down event for the buttons
     @objc func buttonTouchDown(_ sender: UIButton) {
         // Button is being held down (touch down), change background color to red
-        //set up audio model
+        // Set up audio model
         self.audio.play()
         self.audio.startMicrophoneProcessing(withFps: 20) // preferred number of FFT calculations per second
         print("ON \(sender.titleLabel?.text ?? "")")
     }
     
+    // Function to handle the touch up event for the buttons
+    @objc func buttonTouchUpInside(_ sender: UIButton) {
+        // Button is released (touch up), change background color back to normal
+        print("OFF \(sender.titleLabel?.text ?? "")")
+        self.audio.pause()
+        self.server.sendPostRequest(fftData: self.audio.fftData, languageLabel: sender.titleLabel?.text ?? "")
+    }
+    
+    // Function to handle the touch down event for the "Predict" button
     @objc func predicitonButtonTouchDown(_ sender: UIButton) {
         // Button is being held down (touch down), change background color to red
-        //set up audio model
+        // Set up audio model
         self.audio.play()
         self.audio.startMicrophoneProcessing(withFps: 20) // preferred number of FFT calculations per second
         self.finalPrediction.text = "Listening..."
         print("ON \(sender.titleLabel?.text ?? "")")
     }
     
-    @objc func buttonTouchUpInside(_ sender: UIButton) {
-        // Button is released (touch up), change background color back to normal
-        print("OFF \(sender.titleLabel?.text ?? "")")
-        self.audio.pause()
-        self.server.sendPostRequest(fftData: self.audio.fftData, languageLabel: sender.titleLabel?.text ?? "")
-        
-    }
-    
+    // Function to handle the touch up event for the "Predict" button
     @objc func predictButtonTouchUp(_ sender: UIButton){
-        //        self.server.sendPredictionRequest(fftData: self.audio.fftData)
-        //        finalPrediction.text = self.server.predictionLabel
         self.server.sendPredictionRequest(fftData: self.audio.fftData) { [weak self] finalPrediction in
             DispatchQueue.main.async {
                 let startIndex = finalPrediction.index(finalPrediction.startIndex, offsetBy: 2)
@@ -126,15 +137,6 @@ class ViewController: UIViewController, URLSessionDelegate {
                 self?.finalPrediction.text = trimmedPrediction
             }
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
 }
+
